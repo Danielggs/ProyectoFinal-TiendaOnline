015587c487ec25d5db3c7521659e06accb445467
kevin alexis vargas parra <vargasparrakevin@gmail.com>
2022-12-08 19:45:00 -0500

    fomulario producto actualizado

---
+++ src/Components/FormProduct.jsx
@@ -0,0 +1,146 @@
+import React ,{useState} from 'react'
+import './FormProduct.css'
+import Input from './formInput'
+import { crearProducto } from '../Redux/actions'
+import { useSelector , useDispatch} from 'react-redux';
+import { useAuth0 } from "@auth0/auth0-react";
+const FormProduct = () => {
+  const dispatch = useDispatch()
+  const {getAccessTokenSilently } = useAuth0();
+  const[titulo, setTitulo] = useState({campo:'',valido:null})
+  const[precio, setPrecio] = useState({campo:'',valido:null})
+  const[key, setKey] = useState({campo:'',valido:null})
+  const[value, setValue] = useState({campo:'',valido:null})
+  const[stock,setStock]=useState({})
+  const[image, setImage] = useState({campo:'',valido:null})
+  const[validarForm, SetvalidarForm] = useState(null)
+  const expreRP = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+  const expreRN =/^\d{1,14}$/
+  const expreIMG = /^[a-zA-Z\d\s\-\,\#\.\/\_\:\;]{1,200}$/
+
+
+
+
+
+
+
+
+
+
+
+  const onSubmit=async(e)=>{
+    e.preventDefault();
+
+    if(titulo.valido === 'true' && precio.valido === 'true' && image.valido === 'true'){
+      SetvalidarForm(true)
+      
+      const domain = "dev-tsvpp07v3bagspkr.us.auth0.com";
+      const accessToken = await getAccessTokenSilently({
+        audience: `https://${domain}/api/v2/`,
+        scope: "read:current_user",
+      });
+      dispatch(crearProducto ({
+          name:titulo.campo,
+          description: '2',
+          image:image.campo,
+          price:precio.campo,
+          stock:stock,
+          category:"llaveros",
+         },accessToken))
+     
+      setTitulo({campo:'',valido:null})
+      setPrecio({campo:'',valido:null})
+      setValue({campo:"",valido:null})
+      setKey({campo:"",valido:null})
+      setStock({})
+
+
+      setImage({campo:'',valido:null})
+    }else{
+      SetvalidarForm(false)
+    }
+
+  }
+  return (
+    <div className='container'>
+    <form className='form' onSubmit={onSubmit}>
+     
+       <Input
+       estado={titulo}
+       cambiarEstado={setTitulo}
+        titulo='Nombre del Producto'
+        tipo= 'text'
+        Error='caracter no valido'
+        expreReg={expreRP}
+       />
+
+       <Input
+       estado={precio}
+       cambiarEstado={setPrecio}
+        titulo='Precio'
+        tipo= 'text'
+        Error='Caracter no valido'
+        expreReg={expreRN}
+       />
+       <Input
+       estado={image}
+       cambiarEstado={setImage}
+        titulo='image'
+        tipo= 'text'
+        Error='ingrese una imagen'
+        expreReg={expreIMG}
+        
+       />
+
+
+
+       <div className='stock-imputs'>
+        
+        <h1>Stock</h1>
+        {stock&&Object.keys(stock).map((e)=>{
+          return <p>{e+" : "+stock[e]}</p>
+        })}
+        <Input
+          estado={key}
+          cambiarEstado={setKey}
+          titulo='key'
+          tipo='text'
+          Error='ingrese una key valida'
+          expreReg={expreRP}
+        />
+        <Input
+          estado={value}
+          cambiarEstado={setValue}
+          titulo='value'
+          tipo='text'
+          Error='ingrese un value valido'
+          expreReg={expreRN}
+        />
+        <button onClick={()=>{
+          setStock({...stock,[key.campo]:value.campo})
+        }}>add</button>
+        </div>
+
+
+
+
+        <div className='errorContaimner'>
+      {validarForm === false && 
+        <p className="error">Rellene las casillas correctamente</p>
+         }</div>
+
+         
+      <div className='buttonContainer'>
+        <button  className='button'type='submit'> Enviar</button>
+       {validarForm === true && <p className='exito'>El producto se cargo Corretamente</p>}
+        </div>    
+
+    </form>
+    </div>
+  )
+}
+
+export default FormProduct  
+
+
+ 
