022b3e962219d0c5d8f69124d4ccbccbd6198433
kevin alexis vargas parra <vargasparrakevin@gmail.com>
2022-12-13 22:01:03 -0500

    admin

--- src/Components/Carta.js
+++ src/Components/Carta.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useDispatch } from 'react-redux';
 import './card.css'
@@ -12,7 +12,18 @@ const Card = ({data}) => {
   const onClick = (e)=>{
     dispatch(addCart (data))
   }
-    
+  const [stars,setStars]=useState(0);
+  useEffect(()=>{
+    var sum=0;
+    var cont=0;
+    Object.keys(data.rate).map((e)=>{
+      sum+=data.rate[e];
+      cont++;
+    })
+    console.log(sum/cont);
+    setStars(cont>0?sum/cont:1)
+  },[data])
+
   console.log(data)
 
   return (
@@ -23,6 +34,11 @@ const Card = ({data}) => {
     </div>
     <div className="contentBx">
       <h2>{data.name}</h2>
+      <img src={stars<1?'https://upload.wikimedia.org/wikipedia/commons/archive/b/b0/20120722205312%21Star-.svg':'https://upload.wikimedia.org/wikipedia/commons/archive/6/63/20070316213818%21Star%2A.svg'}/>
+      <img src={stars<2?'https://upload.wikimedia.org/wikipedia/commons/archive/b/b0/20120722205312%21Star-.svg':'https://upload.wikimedia.org/wikipedia/commons/archive/6/63/20070316213818%21Star%2A.svg'}/>
+      <img src={stars<3?'https://upload.wikimedia.org/wikipedia/commons/archive/b/b0/20120722205312%21Star-.svg':'https://upload.wikimedia.org/wikipedia/commons/archive/6/63/20070316213818%21Star%2A.svg'}/>
+      <img src={stars<4?'https://upload.wikimedia.org/wikipedia/commons/archive/b/b0/20120722205312%21Star-.svg':'https://upload.wikimedia.org/wikipedia/commons/archive/6/63/20070316213818%21Star%2A.svg'}/>
+      <img src={stars<5?'https://upload.wikimedia.org/wikipedia/commons/archive/b/b0/20120722205312%21Star-.svg':'https://upload.wikimedia.org/wikipedia/commons/archive/6/63/20070316213818%21Star%2A.svg'}/>
       <div className="size">
         <h3>Stock :</h3>
         {
@@ -35,7 +51,6 @@ const Card = ({data}) => {
       </div>
        <div className="color">
         <h3>Precio : {data.price}</h3>
-       
       </div> 
       <button onClick={onClick}>   <AiOutlineShoppingCart  /> agregar al Carrito</button>
     </div>
